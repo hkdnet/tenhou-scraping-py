5bb727ccf7bbf1d56c585f7ccf219aedff5569f7
alek-3 <jaianism.of.grotius@hotmail.co.jp>
2018-01-10 23:04:41 +0900

    インデントを修正

--- scraping.py
+++ scraping.py
@@ -1,5 +1,6 @@
 import bs4
 from operator import itemgetter
+import datetime
 
 output = []
 
@@ -47,52 +48,50 @@ with open('天鳳ランキング_files\dummy.html', 'r', encoding='utf-8') as f:
         if count > 861:
             break
 
-    output = sorted(output, key=lambda x: x['No.'], reverse=True)
-    # print(output)
+output = sorted(output, key=lambda x: x['No.'], reverse=True)
 
-    # 取得されたoutputをベースに分析する
-    analysis = []
-    num_games = count # このとき対戦数 + 1
-    count_first = 0
-    count_second = 0
-    count_third = 0
-    count_fourth = 0
-    
-    for row in output:
-        game_num = num_games - row['No.']
-        # プレーヤーの順位を取得し、回数に追加する。
-        if row['1位'] == player_name:
-            count_first += 1
-        elif row['2位'] == player_name:
-            count_second += 1
-        elif row['3位'] == player_name:
-            count_third += 1
-        else:
-            count_fourth += 1
+# 取得されたoutputをベースに分析する
+analysis = []
+num_games = count # このとき対戦数 + 1
+count_first = 0
+count_second = 0
+count_third = 0
+count_fourth = 0
 
-        rank_times = {
-            'No.': game_num,
-            '1位': count_first,
-            '2位': count_second,
-            '3位': count_third,
-            '4位': count_fourth
-        }
+for row in output:
+    game_num = num_games - row['No.']
+    # プレーヤーの順位を取得し、回数に追加する。
+    if row['1位'] == player_name:
+        count_first += 1
+    elif row['2位'] == player_name:
+        count_second += 1
+    elif row['3位'] == player_name:
+        count_third += 1
+    else:
+        count_fourth += 1
 
-        if game_num > 100:
-            rank_times_bef100 = analysis[game_num - 100 - 1] # 101ゲーム目の場合、1ゲーム目を取得
-            rank_times['直近1位'] = count_first - rank_times_bef100['1位']
-            rank_times['直近2位'] = count_second  - rank_times_bef100['2位']
-            rank_times['直近3位'] = count_third - rank_times_bef100['3位']
-            rank_times['直近4位'] = count_fourth - rank_times_bef100['4位']
+    rank_times = {
+        'No.': game_num,
+        '1位': count_first,
+        '2位': count_second,
+        '3位': count_third,
+        '4位': count_fourth
+    }
 
-        analysis.append(rank_times)
-    # print(analysis)
+    if game_num > 100:
+        rank_times_bef100 = analysis[game_num - 100 - 1] # 101ゲーム目の場合、1ゲーム目を取得
+        rank_times['直近1位'] = count_first - rank_times_bef100['1位']
+        rank_times['直近2位'] = count_second  - rank_times_bef100['2位']
+        rank_times['直近3位'] = count_third - rank_times_bef100['3位']
+        rank_times['直近4位'] = count_fourth - rank_times_bef100['4位']
+
+    analysis.append(rank_times)
 
 # 分析結果をcsvファイルに出力する。
 with open('output.csv', 'w', encoding='utf-8') as f:
     # ヘッダー
     f.write('No.,1位,2位,3位,4位,直近1位,直近2位,直近3位,直近4位\n')
-    
+
     for row in analysis:
         f.write(str(row['No.']))
         f.write(',')
